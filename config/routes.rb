cd96ca1e142fa09b9e23ae7d51d10f55af0c1736
Omega B. Dziko Fuentes <omegab.dzikofuentes@MacBookAir.lan>
2025-04-11 12:23:12 -0400

    routes work

--- config/routes.rb
+++ config/routes.rb
@@ -13,16 +13,5 @@ Rails.application.routes.draw do
   # root "posts#index"
   root "products#index"
   
-  get "/products", to: "products#index"
-
-  get "/products/new", to: "products#new"
-  post "/products", to: "products#create"
-
-  get "/products/:id", to: "products#show"
-
-  get "/products/:id/edit", to: "products#edit"
-  patch "/products/:id", to: "products#update"
-  put "/products/:id", to: "products#update"
-
-  delete "/products/:id", to: "products#destroy"
+  resources :products
 end
