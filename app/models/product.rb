20f7ce73b159da8cc07830b0fa07afb364790b8e
Omega B. Dziko Fuentes <omegab.dzikofuentes@MacBookAir.lan>
2025-04-14 12:41:31 -0400

    testing mailers

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,19 @@ class Product < ApplicationRecord
     validates :inventory_count, numericality: {
         greater_than_or_equal_to: 0
     }
+
+    after_update_commit :notify_subscribers, if: :back_in_stock?
+
+    def back_in_stock?
+        saved_change_to_inventory_count? && 
+        inventory_count_before_last_save.to_i.zero? && 
+        inventory_count > 0
+    end
+
+    def notify_subscribers
+        subscribers.each do |subscriber|
+            ProductMailer.with(product: self, subscriber:
+             subscriber).in_stock.deliver_later
+        end
+    end
 end
